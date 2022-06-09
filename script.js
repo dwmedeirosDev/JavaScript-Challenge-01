342302a2e80a1670cb84591978342fd3df334f98
dwmedeiros94 <david.william94@live.com>
2022-06-09 15:31:42 -0300

    OK

---
+++ script.js
@@ -0,0 +1,16 @@
+let nome = "Sala 1";
+
+let number = 30;
+
+let ano = 2022;
+console.log(ano-number);
+
+console.log(ano+number);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = true;
+console.log(quartaFeira);
+
+console.log(typeof quartaFeira);
+
